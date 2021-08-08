0ae9765926d679b71c4fd89ff99235aff3814e17
Arruda2904 <gabriel.dossantos.ismart@gmail.com>
2021-08-08 16:20:38 -0300

    Iniciando sem hash

---
+++ main.c
@@ -0,0 +1,28 @@
+int main(int argc, char **argv)
+{
+    int tok;
+    if(argc < 2)
+    {
+        fprintf(stderr,"Call: ./a.out file_name\n");
+        exit(1);
+    }
+    yyin = fopen(argv[1], "r");
+
+    while(running)
+    {  
+        tok = yylex();
+        if(!running)
+        {
+            break;
+        }
+        switch(tok)
+        {
+            case KW_INT: printf("Encontrei KW_INT\n"); break;
+            case LIT_INTEGER: printf("Encontrei LIT_INT\n"); break;
+            case TOKEN_ERROR: printf("Unexpected tokken %d\n",tok); break;
+            default: printf("Encontrei %c\n", yytext[0]); break;
+        }
+    }
+    printf("Main done! File has %d lines.\n", lineNumber);
+    exit(0);
+}
